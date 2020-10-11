b4f0c92e2e260cfca4bf9d9e637a7e1e510ae24f
aimarket <newbassvids@gmail.com>
2020-10-10 23:35:53 -0700

    website submission

--- scripts/start.js
+++ scripts/start.js
@@ -1,12 +1,12 @@
 window.onload = function() {
   Particles.init({
     selector: '.background',
-    color: ['#fc0303', '#18fc03', '#03f8fc', '#ff00f2'],
+    color: ['#fc0303', '#ffffff', '0026ff', '#000000'],
     connectParticles: true,
     responsive: [{
       breakpoint: 700,
       options: {
-        color: ['#fc0303', '#18fc03', '#03f8fc', '#ff00f2'],
+        color: ['#fc0303', '#ffffff', '0026ff', '#000000'],
         maxParticles: 50,
         connectParticles: true,
         speed: .3,
@@ -14,4 +14,3 @@ window.onload = function() {
     }]
   });
 };
-
